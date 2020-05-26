f7a671cf3e0d1d25210eb3718ac408018fb4151a
Anil Gurung <anil_gurung15@yahoo.com>
2020-05-26 08:15:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'products/index'
+  
+  resources :products
+  root 'products#index'
 
-  post '/', to: 'products#add'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
